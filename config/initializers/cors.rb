9663b58cba838ffe87d120796e12f88c52f068b7
didi <imdrchaka@gmail.com>
2019-01-04 18:56:14 +0200

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:4200','https://the-pm-production.herokuapp.com']
+    origins 'http://localhost:4200','https://the-pm-production.herokuapp.com'
 
     resource '*',
       headers: :any,
